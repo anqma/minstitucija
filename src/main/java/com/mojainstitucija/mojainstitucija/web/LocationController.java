c54466cb05ed00edb36f9efb94b351f15a597ad2
anqma <94206383+anqma@users.noreply.github.com>
2021-12-21 15:58:08 +0100

    .

--- src/main/java/com/mojainstitucija/mojainstitucija/web/LocationController.java
+++ src/main/java/com/mojainstitucija/mojainstitucija/web/LocationController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/institucii")
+@RequestMapping("/")
 public class LocationController {
     private final KategorijaService kategorijaService;
     private final UslugaService uslugaService;
